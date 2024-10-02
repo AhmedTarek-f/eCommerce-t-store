dcdf1d890e046d2019a7ef0147607c6970e9ac51
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-10-02 14:48:02 +0300

    adding verify email controller

---
+++ lib/features/auth/verify_email/presentation/views_model/verify_email_controller.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:t_store/common_widgets/success_screen.dart';
+import 'package:t_store/core/constants/image_strings.dart';
+import 'package:t_store/core/utlis/loaders/t_loaders.dart';
+import 'package:t_store/data/repositories/authentication/authentication_repository.dart';
+
+class VerifyEmailController extends GetxController
+{
+  static VerifyEmailController get instance => Get.find();
+
+  // Send Email When ever Verify Screen appears & set timer for auto redirect
+  @override
+  void onInit() {
+    sendEmailVerification();
+    setTimerForAutoRedirect();
+    super.onInit();
+
+  }
+
+  // Send Email Verification link
+ Future<void> sendEmailVerification()async{
+    try{
+      await AuthenticationRepository.instance.sendEmailVerification();
+      TLoaders.successSnackBar(title: "Email sent",message: "Please check your inbox and verify your email.");
+    }
+    catch(e)
+   {
+     TLoaders.errorSnackBar(title: "Oh Snap!",message: e.toString());
+   }
+  }
+
+  // Timer to automatically redirect on Email Verification
+  Future<void> setTimerForAutoRedirect() async
+  {
+    Timer.periodic(
+        const Duration(seconds: 1),
+        (timer) async{
+          await FirebaseAuth.instance.currentUser?.reload();
+          final user = FirebaseAuth.instance.currentUser;
+          if(user?.emailVerified ?? false)
+            {
+              timer.cancel();
+              Get.off(()=>SuccessScreen(
+                isAnimation: true,
+                image: TImages.successfullyRegisterAnimation,
+                title:"Your account successfully created!",
+                subTitle: "Welcome to Your Ultimate Shopping Destination Your Account is Created, Unleash the Joy of Seamless Online Shopping!",
+                onPressed: ()=> AuthenticationRepository.instance.screenRedirect(),
+              )
+              );
+            }
+        },
+    );
+  }
+  // Manually check if email verified
+  Future<void> checkEmailVerificationStatus() async{
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if(currentUser!=null && currentUser.emailVerified)
+    {
+      Get.off(()=>SuccessScreen(
+        isAnimation: true,
+        image: TImages.successfullyRegisterAnimation,
+        title:"Your account created",
+        subTitle: "",
+        onPressed: ()=> AuthenticationRepository.instance.screenRedirect(),
+      )
+      );
+    }
+  }
+
+}
