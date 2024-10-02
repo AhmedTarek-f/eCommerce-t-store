d8c6735933ae4fdac7fd24164a85549b81e64727
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-10-02 15:01:08 +0300

    adding logic to login view

--- lib/features/auth/log_in/presentation/views/widgets/log_in_view_body.dart
+++ lib/features/auth/log_in/presentation/views/widgets/log_in_view_body.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:t_store/common_widgets/t_form_divider.dart';
 import 'package:t_store/common_widgets/t_social_buttons.dart';
+import 'package:t_store/data/repositories/authentication/authentication_repository.dart';
 import 'package:t_store/features/auth/log_in/presentation/views/widgets/t_log_in_form.dart';
 import 'package:t_store/features/auth/log_in/presentation/views/widgets/t_log_in_header.dart';
+import 'package:t_store/features/auth/log_in/presentation/views_model/log_in_controller.dart';
 
 
 class LogInViewBody extends StatelessWidget {
@@ -10,6 +13,7 @@ class LogInViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final LogInController logInController = Get.put(LogInController());
     return  SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.only(
@@ -18,13 +22,17 @@ class LogInViewBody extends StatelessWidget {
           right: MediaQuery.sizeOf(context).width *0.0611,
           left: MediaQuery.sizeOf(context).width *0.0611,
         ),
-        child: const Column(
+        child:  Column(
           children: [
-             TLogInHeader(),
-             TLogInForm(),
-             TFormDivider(dividerText:"Or Sign In With"),
-             SizedBox(height: 32,),
-             TSocialButtons()
+            const TLogInHeader(),
+            const TLogInForm(),
+            const TFormDivider(dividerText:"Or Sign In With"),
+            const SizedBox(height: 32,),
+             TSocialButtons(
+               googleOnPressed: ()async{
+                 await logInController.googleSignIn();
+               },
+             )
           ],
         ),
       ),
--- lib/features/auth/log_in/presentation/views/widgets/t_log_in_form.dart
+++ lib/features/auth/log_in/presentation/views/widgets/t_log_in_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:t_store/features/auth/log_in/presentation/views_model/log_in_controller.dart';
 import 'package:t_store/features/auth/password_configuration/presentation/views/forget_password_view.dart';
 import 'package:t_store/features/auth/sign_up/presentation/views/sign_up_view.dart';
 import 'package:t_store/features/shop/navigation_menu/presentation/views/navigation_menu_view.dart';
@@ -12,69 +13,102 @@ class TLogInForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 32),
-          child: Form(
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration:InputDecoration(
-                    prefixIcon: const Icon(Iconsax.direct_right),
-                    label: Text("E-Mail",style:Theme.of(context).textTheme.bodyMedium ,),
-                  ),
-                ),
-                const SizedBox(height: 16,),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      prefixIcon: const Icon(Iconsax.password_check),
-                      label: Text("Password",style: Theme.of(context).textTheme.bodyMedium,),
-                      suffixIcon: const Icon(Iconsax.eye_slash)
-                  ),
-                ),
-                const SizedBox(height: 8,),
-                Row(
-                  mainAxisAlignment:  MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Checkbox(value: true, onChanged: (value){}),
-                        const Text("Remember Me")
-                      ],
+    return GetBuilder<LogInController>(
+      init: LogInController(),
+      builder: (controller) =>Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32),
+            child: Form(
+              key: controller.loginFormKey,
+              autovalidateMode: controller.autoValidateMode,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration:InputDecoration(
+                      prefixIcon: const Icon(Iconsax.direct_right_copy),
+                      label: Text("E-Mail",style:Theme.of(context).textTheme.bodyMedium ,),
                     ),
-                    TextButton(onPressed: (){
-                      Get.to(()=>const ForgetPasswordView());
+                    controller: controller.email,
+                    validator: (value) {
+                      final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+                      if(value == null || value.isEmpty || value.trim() == ""){
+                        return "Email is required.";
+                      }
+                      else if(!emailRegExp.hasMatch(value))
+                      {
+                        return "Invalid email address.";
+                      }
+                      else
+                      {
+                        return null;
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 16,),
+                  TextFormField(
+                    obscureText: controller.hidePassword,
+                    decoration: InputDecoration(
+                        prefixIcon: const Icon(Iconsax.password_check_copy),
+                        label: Text("Password",style: Theme.of(context).textTheme.bodyMedium,),
+                        suffixIcon:controller.hidePassword?
+                        IconButton(onPressed: (){controller.hideShowPassword();}, icon: const Icon(Iconsax.eye_slash_copy)):
+                        IconButton(onPressed: (){controller.hideShowPassword();}, icon: const Icon(Iconsax.eye_copy))
+                    ),
+                    controller: controller.password,
+                    validator: (value) {
+                      if(value == null || value.isEmpty || value.trim() == "")
+                      {
+                        return "Password is required.";
+                      }
+                      else
+                      {
+                        return null;
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 8,),
+                  Row(
+                    mainAxisAlignment:  MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Checkbox(value: controller.localStorage.read("REMEMBER_ME_VALUE")??false, onChanged: (_){controller.rememberMeSignChange();}),
+                          const Text("Remember Me")
+                        ],
+                      ),
+                      TextButton(onPressed: (){
+                        Get.to(()=>const ForgetPasswordView());
                       },
-                        child: const Text("Forget Password?")),
-                  ],
-                ),
-              ],
+                          child: const Text("Forget Password?")),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          width:MediaQuery.sizeOf(context).width,
-          child: ElevatedButton(
-            onPressed: (){
-              Get.to(()=>const NavigationMenuView(),);
+          SizedBox(
+            width:MediaQuery.sizeOf(context).width,
+            child: ElevatedButton(
+              onPressed: (){
+                controller.loginWithEmailAndPassword();
               },
-            child: const Text("Sign In"),
+              child: const Text("Sign In"),
+            ),
           ),
-        ),
-        const SizedBox(height: 16,),
-        SizedBox(
-          width:MediaQuery.sizeOf(context).width,
-          child: OutlinedButton(
-            onPressed: (){
-              Get.to(()=>const SignUpView());
-            },
-            child: const Text("Create Account"),
+          const SizedBox(height: 16,),
+          SizedBox(
+            width:MediaQuery.sizeOf(context).width,
+            child: OutlinedButton(
+              onPressed: (){
+                Get.to(()=>const SignUpView());
+              },
+              child: const Text("Create Account"),
+            ),
           ),
-        ),
-        const SizedBox(height: 32,),
-      ],
+          const SizedBox(height: 32,),
+        ],
+      ) ,
     );
   }
 }
