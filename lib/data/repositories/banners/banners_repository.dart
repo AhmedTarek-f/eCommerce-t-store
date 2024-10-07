108f0a1decdf4d514c8259d7de1b74100a9d3183
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-10-07 18:58:13 +0300

    adding categories & banners repositories

---
+++ lib/data/repositories/banners/banners_repository.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:t_store/core/utlis/exceptions/t_firebase_exceptions.dart';
+import 'package:t_store/core/utlis/exceptions/t_platform_exceptions.dart';
+import 'package:t_store/features/shop/home/model/banner_model.dart';
+
+class BannersRepository extends GetxController {
+  static BannersRepository get instance => Get.find();
+
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Future<List<BannerModel>> getAllBanners() async {
+    try{
+      final snapshot = await _db.collection("Banners").where("Active",isEqualTo: true).get();
+      final List<BannerModel> allBannersList = snapshot.docs.map((bannerDocument) => BannerModel.fromSnapshot(bannerDocument)).toList();
+      return allBannersList;
+    } on FirebaseException catch (e){
+      throw TFirebaseException(e.code).message;
+    }
+    on PlatformException catch(e)
+    {
+      throw TPlatformException(e.code).message;
+    }
+    catch (e)
+    {
+      throw "Something went wrong, Please try again";
+    }
+
+  }
+}
---
+++ lib/data/repositories/categories/category_repository.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:t_store/core/utlis/exceptions/t_firebase_exceptions.dart';
+import 'package:t_store/core/utlis/exceptions/t_platform_exceptions.dart';
+import 'package:t_store/features/shop/home/model/category_model.dart';
+
+class CategoryRepository extends GetxController {
+  static CategoryRepository get instance => Get.find();
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Future<List<CategoryModel>> getAllCategories() async {
+    try{
+      final snapshot = await _db.collection("Categories").get();
+      final allCategoriesList = snapshot.docs.map((document) => CategoryModel.fromSnapShot(document)).toList();
+      return allCategoriesList;
+    }
+    on FirebaseException catch (e){
+      throw TFirebaseException(e.code).message;
+    }
+    on PlatformException catch(e)
+    {
+      throw TPlatformException(e.code).message;
+    }
+    catch (e)
+    {
+      throw "Something went wrong, Please try again";
+    }
+  }
+}
