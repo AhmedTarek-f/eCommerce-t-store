4cdc6fac8d6683292c8be0a7cdcba6e1f9633c17
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-10-10 15:12:48 +0300

    adding all products controller and all products view logic

--- lib/features/shop/all_products/presentation/views/all_products_view.dart
+++ lib/features/shop/all_products/presentation/views/all_products_view.dart
@@ -1,18 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:t_store/common_widgets/t_app_bar/t_app_bar.dart';
 import 'package:t_store/features/shop/all_products/presentation/views/widgets/all_products_view_body.dart';
+import 'package:t_store/features/shop/product_details/model/product_model.dart';
 
 class AllProductsView extends StatelessWidget {
-  const AllProductsView({super.key});
-
+  const AllProductsView({
+    super.key,
+    required this.title,
+    this.query,
+    this.futureMethod
+  });
+  final String title;
+  final Query? query;
+  final Future<List<ProductModel>>? futureMethod;
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: TAppBar(
-        title: Text("Popular Products",style: Theme.of(context).textTheme.headlineSmall,),
+        title: Text(title,style: Theme.of(context).textTheme.headlineSmall,),
         showBackArrow: true,
       ),
-      body: const AllProductsViewBody(),
+      body: AllProductsViewBody(
+        query: query,
+        futureMethod: futureMethod,
+      ),
     );
   }
 }
--- lib/features/shop/all_products/presentation/views/widgets/all_products_view_body.dart
+++ lib/features/shop/all_products/presentation/views/widgets/all_products_view_body.dart
@@ -1,9 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:t_store/common_widgets/products/sortable/t_sortable_products.dart';
+import 'package:t_store/features/shop/all_products/presentation/views_model/all_products_controller.dart';
+import 'package:t_store/features/shop/home/presentation/views/widgets/t_vertical_product_shimmer.dart';
+import 'package:t_store/features/shop/product_details/model/product_model.dart';
 
 class AllProductsViewBody extends StatelessWidget {
-  const AllProductsViewBody({super.key});
-
+  const AllProductsViewBody({super.key, this.query, this.futureMethod});
+  final Query? query;
+  final Future<List<ProductModel>>? futureMethod;
   final List<String> searchListOptions = const [
     "Name",
     "Higher Price",
@@ -14,6 +20,7 @@ class AllProductsViewBody extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
+    final AllProductsController controller = Get.put(AllProductsController());
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.only(
@@ -22,7 +29,25 @@ class AllProductsViewBody extends StatelessWidget {
           top: MediaQuery.sizeOf(context).height*0.0280,
           bottom: MediaQuery.sizeOf(context).height*0.0280,
         ),
-        child: TSortableProducts(searchListOptions: searchListOptions),
+        child: FutureBuilder<List<ProductModel>>(
+            future: futureMethod ?? controller.fetchProductsByQuery(query),
+            builder: (BuildContext context, AsyncSnapshot<List<ProductModel>> snapshot) {
+              if(snapshot.connectionState == ConnectionState.waiting) {
+                return const TVerticalProductShimmer();
+              }
+              else if(!snapshot.hasData || snapshot.data == null || snapshot.data!.isEmpty){
+                return const Center(child:  Text("No Data Found!"),);
+              }
+              else if(snapshot.hasError)
+                {
+                  return const Center(child:  Text("Something went wrong."),);
+                }
+              else{
+                final List<ProductModel> products = snapshot.data!;
+                return TSortableProducts(searchListOptions: searchListOptions, products: products,);
+              }
+            },
+        ),
       ),
     );
   }
