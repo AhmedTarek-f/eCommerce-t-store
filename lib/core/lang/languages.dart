ce78da2d167ab5bc10248ba80a591125d6fd9781
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-10-17 17:06:34 +0300

    adding language

---
+++ lib/core/lang/languages.dart
@@ -0,0 +1,69 @@
+import 'package:get/get.dart';
+
+class Languages implements Translations
+{
+  @override
+  // TODO: implement keys
+  Map<String, Map<String, String>> get keys => {
+    "ar": {
+      "Skip": "تخطي",
+      "Choose your product" : "اختر منتجك",
+      "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!": "مرحبًا بكم في عالم من الاختيارات اللامحدودة - منتجك المثالي في انتظارك!",
+      "Select Payment Method" : "حدد طريقة الدفع",
+      "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!":"لإجراء معاملات سلسة، اختر مسار الدفع الخاص بك - راحتك، أولويتنا!",
+      "Deliver at your door step":"التوصيل إلى باب منزلك",
+      "From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!":"من باب منزلنا إلى باب منزلك - تسليم سريع وآمن وبدون تلامس!",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+
+    },
+    "en": {
+      "Skip": "Skip",
+      "Choose your product": "Choose your product",
+      "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!": "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!",
+      "Select Payment Method" : "Select Payment Method",
+      "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!":"For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!",
+      "Deliver at your door step":"Deliver at your door step",
+      "From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!":"From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+      "":"",
+    }
+  };
+
+}
