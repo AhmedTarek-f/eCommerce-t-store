beb2f7c4fb9375c40407c9d7e9bb00bddf7bc78a
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-10-17 14:51:42 +0300

    adding checkout controller

---
+++ lib/features/shop/checkout/presentation/views_model/checkout_controller.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:t_store/common_widgets/texts/t_section_heading.dart';
+import 'package:t_store/core/constants/image_strings.dart';
+import 'package:t_store/features/shop/checkout/model/payment_method_model.dart';
+import 'package:t_store/features/shop/checkout/presentation/views/widgets/t_payment_tile.dart';
+
+class CheckoutController extends GetxController {
+  static CheckoutController get instance => Get.find();
+
+  final Rx<PaymentMethodModel> selectedPaymentMethod = PaymentMethodModel.empty().obs;
+
+  @override
+  void onInit() {
+    selectedPaymentMethod.value = const PaymentMethodModel(image: TImages.paypal, name: "Paypal");
+    super.onInit();
+  }
+
+  final List<PaymentMethodModel> _paymentMethodsList =  const [
+    PaymentMethodModel(image: TImages.paypal, name: "Paypal"),
+    PaymentMethodModel(image: TImages.creditCard, name: "CreditCard"),
+    PaymentMethodModel(image: TImages.visa, name: "Visa"),
+    PaymentMethodModel(image: TImages.masterCard, name: "MasterCard"),
+    PaymentMethodModel(image: TImages.googlePay, name: "GooglePay"),
+    PaymentMethodModel(image: TImages.applePay, name: "ApplePay"),
+    PaymentMethodModel(image: TImages.payStack, name: "PayStack"),
+    PaymentMethodModel(image: TImages.paytm, name: "Paytm"),
+  ];
+
+  Future<dynamic> selectPaymentMethod(BuildContext context){
+    return showModalBottomSheet(
+        context: context,
+        builder: (context) => SingleChildScrollView(
+          child: Container(
+            padding:  const EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const TSectionHeading(title: "Select Payment Method" ,showActionButton: false,),
+                const SizedBox(height: 32,),
+                Column(
+                  children: _paymentMethodsList.map((paymentMethod) => Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: TPaymentTile(paymentMethod: paymentMethod),
+                  )).toList(),
+                )
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+}
