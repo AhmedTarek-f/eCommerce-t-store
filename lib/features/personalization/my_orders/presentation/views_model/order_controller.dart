1b6b6cd0c2b3e48061d55c6c30efe059070c9906
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-10-17 14:53:50 +0300

    adding order controller

---
+++ lib/features/personalization/my_orders/presentation/views_model/order_controller.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:t_store/common_widgets/success_screen.dart';
+import 'package:t_store/core/constants/enums.dart';
+import 'package:t_store/core/constants/image_strings.dart';
+import 'package:t_store/core/utlis/loaders/t_loaders.dart';
+import 'package:t_store/core/utlis/popups/t_full_screen_loader.dart';
+import 'package:t_store/data/repositories/authentication/authentication_repository.dart';
+import 'package:t_store/data/repositories/order/order_repository.dart';
+import 'package:t_store/features/personalization/my_address/presentation/views_model/address_controller.dart';
+import 'package:t_store/features/personalization/my_orders/model/order_model.dart';
+import 'package:t_store/features/shop/cart/presentation/views_model/cart_controller.dart';
+import 'package:t_store/features/shop/checkout/presentation/views_model/checkout_controller.dart';
+import 'package:t_store/features/shop/navigation_menu/presentation/views/navigation_menu_view.dart';
+
+class OrderController extends GetxController
+{
+  static OrderController get instance => Get.find();
+
+  final cartController = CartController.instance;
+  final addressController = AddressController.instance;
+  final checkoutController = CheckoutController.instance;
+  final _orderRepository = Get.put(OrderRepository());
+
+  Future<List<OrderModel>> fetchUserOrders() async {
+    try {
+      final userOrders = await _orderRepository.fetchUserOrders();
+      return userOrders;
+    }
+    catch(e) {
+      TLoaders.errorSnackBar(title: "Oh Snap!",message: e.toString());
+      return [];
+    }
+  }
+
+  Future<void> processOrder(double totalAmount) async {
+    try{
+      TFullScreenLoader.openLoadingDialog("Processing your order", TImages.pencilAnimation);
+
+      final userId = AuthenticationRepository.instance.authUser!.uid;
+      if(userId.isEmpty) return;
+
+      final order = OrderModel(
+          id: UniqueKey().toString(),
+          userId: userId,
+          status: OrderStatus.pending,
+          totalAmount: totalAmount,
+          orderDate: DateTime.now(),
+          paymentMethod: checkoutController.selectedPaymentMethod.value.name,
+          address: addressController.selectedAddress.value,
+          deliveryDate: DateTime.now(),
+          items: cartController.cartItems.toList(),
+      );
+
+      await _orderRepository.saveOrder(order, userId);
+
+      cartController.clearCart();
+      
+      Get.off(() => SuccessScreen(
+        image: TImages.orderCompleteAnimation,
+        title: "Payment Success!",
+        subTitle: "Your item will be shipped soon!",
+        isAnimation: true,
+        onPressed: () => Get.offAll(() => const NavigationMenuView()),
+      )
+      );
+    }
+    catch(e) {
+      TLoaders.errorSnackBar(title: "Oh Snap!",message: e.toString());
+    }
+  }
+
+}
