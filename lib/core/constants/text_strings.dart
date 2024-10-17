f6d8b5340653bbb831fdd7bc3d916a369c51c491
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-10-17 20:38:49 +0300

    modified version

--- lib/core/constants/text_strings.dart
+++ lib/core/constants/text_strings.dart
@@ -9,7 +9,4 @@ abstract class TTexts {
   static String onBoardingSubTitle1 = "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!".tr;
   static String onBoardingSubTitle2 = "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!".tr;
   static String onBoardingSubTitle3 = "From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!".tr;
-
-// -- Home
-  static String homeAppbarTitle = "Good day for shopping";
 }
