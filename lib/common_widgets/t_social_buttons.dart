b153bd693b7b2eeacd3b7e954f2440bc0c040efe
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-10-21 21:58:06 +0300

    removing facebook button

--- lib/common_widgets/t_social_buttons.dart
+++ lib/common_widgets/t_social_buttons.dart
@@ -9,33 +9,18 @@ class TSocialButtons extends StatelessWidget {
   final void Function()? facebookOnPressed;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          decoration:  BoxDecoration(border:Border.all(color: Colors.grey) ,borderRadius: BorderRadius.circular(100)),
-          child: IconButton(
-            onPressed: googleOnPressed,
-            icon: Image.asset(
-              TImages.google,
-              width: MediaQuery.sizeOf(context).width*0.0611,
-              height: MediaQuery.sizeOf(context).height*0.028,
-            ),
+    return Center(
+      child: Container(
+        decoration:  BoxDecoration(border:Border.all(color: Colors.grey) ,borderRadius: BorderRadius.circular(100)),
+        child: IconButton(
+          onPressed: googleOnPressed,
+          icon: Image.asset(
+            TImages.google,
+            width: MediaQuery.sizeOf(context).width*0.0611,
+            height: MediaQuery.sizeOf(context).height*0.028,
           ),
         ),
-        const SizedBox(width: 16,),
-        Container(
-          decoration:  BoxDecoration(border: Border.all(color: Colors.grey),borderRadius: BorderRadius.circular(100)),
-          child: IconButton(
-            onPressed: facebookOnPressed,
-            icon: Image.asset(
-              TImages.facebook,
-              width: 24,
-              height: 24,
-            ),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
